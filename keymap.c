a7a336390ae15d2950e42703efc434469f2885c4
Sportsbadger <68499670+Sportsbadger@users.noreply.github.com>
2024-03-24 22:41:18 +0000

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2023 @ Keychron (https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "quantum.h"
 
@@ -25,60 +9,56 @@ enum layers {
 };
 
 enum custom_keycodes {
-    M_ALT_TAB = SAFE_RANGE
+    ALT_TAB = SAFE_RANGE
 };
-
+3........
 //Tap Dance Declarations
 
 enum {
-  TD_ESC_CAPS = 0
+  TD_PASTE_PASTESPECIAL = 0,
+  TD_FIND_REPLACE
 };
 
 //Tap Dance Definitions
 tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Esc, twice for Caps Lock
-  [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
+  [TD_PASTE_PASTESPECIAL]  = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_V), LALT(LCTL(KC_V))),
+  [TD_FIND_REPLACE]  = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_F), LCTL(KC_H))
 // Other declarations would go here, separated by commas, if you have them
 };
 
 
-static bool is_alt_tab_active = false;
-static uint16_t alt_tab_timer;
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case M_ALT_TAB:
+        case ALT_TAB:
             if (record->event.pressed) {
-                // Key pressed
-                if (!is_alt_tab_active) {
-                    is_alt_tab_active = true;
-                    register_code(KC_LALT);
-                }
-                alt_tab_timer = timer_read();
+                // When keycode ALT_TAB is pressed
+                register_code(KC_LALT);
                 tap_code(KC_TAB);
+                // Keeping ALT pressed until the key is released
             } else {
-                // Key released
-                if (timer_elapsed(alt_tab_timer) > 500) { // Adjust the timing to your liking
-                    unregister_code(KC_LALT);
-                    is_alt_tab_active = false;
-                }
+                // When keycode is released, ALT is released
+                unregister_code(KC_LALT);
             }
-            break; // This break corresponds to the switch statement
-    } // This closing bracket ends the switch statement block
+            break;
+    }
     return true;
 }
 
 
 
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_numpad_6x5(
-        KC_MUTE,    LCTL(KC_N), TD(TD_ESC_CAPS),LCTL(KC_A),    MO(FUNC),
-        M_ALT_TAB,  LWIN(KC_D), LALT(KC_F4),    LCTL(KC_F),    LCTL(KC_M),
+        KC_MUTE,    LCTL(KC_N), KC_NO,LCTL(KC_A),    MO(FUNC),
+        ALT_TAB,  LWIN(KC_D), LALT(KC_F4),    TD(TD_FIND_REPLACE),    LCTL(KC_M),
         LCTL(KC_Z), KC_P7,      KC_P8,          KC_P9,         KC_PPLS,
         LCTL(KC_X), KC_P4,      KC_P5,          KC_P6,
         LCTL(KC_C), KC_P1,      KC_P2,          KC_P3,         KC_PENT,
-        LCTL(KC_V), KC_P0,                      KC_PDOT),
+        TD(TD_PASTE_PASTESPECIAL), KC_P0,                      KC_PDOT),
 
     [FUNC] = LAYOUT_numpad_6x5(
         RGB_TOG, _______,  KC_MUTE, KC_VOLD,  KC_VOLU,
